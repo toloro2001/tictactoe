d8ad3958c808979b5bf19418c7cf3499656a75b2
toloro2001 <rotonton000@warren.k12.in.us>
2018-07-02 11:53:43 -0400

    9

--- tictactoe.js
+++ tictactoe.js
@@ -15,17 +15,9 @@ const winningCombinations = [
     [1, 5, 9],
     [3, 5, 7]
   ]
-        <td id="1"></td>
-        <td id="2"></td>
-        <td id="3"></td>
-        <td id="4"></td>
-        <td id="5"></td>
-        <td id="6"></td>
-        <td id="7"></td>
-        <td id="8"></td>
-        <td id="9"></td>
+
   handleClick = function(event) {
-    var cell = event.target
+    var cell = event.click
     console.log("cell Id"+cell.id);
   }
   
