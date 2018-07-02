ec1a7337186002c4c8a7f016c71e30e80379273b
toloro2001 <rotonton000@warren.k12.in.us>
2018-07-02 11:37:31 -0400

    5

--- tictactoe.js
+++ tictactoe.js
@@ -5,23 +5,37 @@ var nextPlayer = "O";
 var playerXSelections = new Array();
 var playerOSelections = new Array();
 const winningCombinations = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-    [1, 4, 7],
-    [2, 5, 8],
-    [3, 6, 9],
-    [1, 5, 9],
-    [3, 5, 7]
-  ]
-  handleClick = function(event) {
-    var cell = event.target
-    console.log(cell.id);
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9],
+  [1, 5, 9],
+  [3, 5, 7]
+]
+handleClick = function (event) {
+  var cell = event.target;
+
+  cell.innerHTML = currentPlayer;
+
+  if (currentPlayer === "X") {
+    playerSelections = playerXSelections;
+    nextPlayer = "O";
+  } else {
+    playerSelections = playerOSelections;
+    nextPlayer = "X";
   }
-  
-  var cells = document.querySelectorAll("td");
-  
-  for(var i = 0; i < cells.length; i++) {
-    cells[i].addEventListener('click', handleClick)
-  }
-  
+
+  playerSelections.push(parseInt(cell.id));
+
+  // Swap players
+  currentPlayer = nextPlayer;
+
+}
+
+var cells = document.querySelectorAll("td");
+
+for (var i = 0; i < cells.length; i++) {
+  cells[i].addEventListener('click', handleClick)
+}
